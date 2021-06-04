ccf044c89777c0b630ed194898df45398f1147d6
Somphoph <somphop.homprasert@krungthai.com>
2021-06-04 17:36:56 +0700

    Add AuthenticationController and WebSecurityConfig

---
+++ src/main/java/com/example/demoauthentication/controller/AuthenticationController.java
@@ -0,0 +1,19 @@
+package com.example.demoauthentication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthenticationController {
+
+    @GetMapping("/authorize")
+    public String authorize(@RequestParam("client_id") String clientId, @RequestParam("redirect_url") String redirectUrl, @RequestParam String state, @RequestParam("code_challenge") String codeChallenge) {
+        return "signin";
+    }
+    @PostMapping("/login")
+    public String signIn(){
+        return "";
+    }
+}
